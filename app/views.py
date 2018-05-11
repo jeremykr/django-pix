94a742a0224a3386ca4ca8f00c2e9fd3d67d7902
jeremykr <jeremy.krenbrink@gmail.com>
2018-05-11 17:00:59 -0600

    Initial commit with image uploader

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+from .forms import UploadImageForm
+
+def index(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        print(form.errors)
+        if form.is_valid():
+            return HttpResponse('Hey, thanks!')
+        else:
+            return HttpResponse('Wow, no thanks.')
+        
+    elif request.method == 'GET':
+        template = loader.get_template('index.html')
+        form = UploadImageForm()
+        return HttpResponse(template.render({'form': form}, request))
